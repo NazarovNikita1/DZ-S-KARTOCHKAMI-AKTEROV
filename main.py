95b6d193872ab50dc1f36af518eb62aa0ccde99c
NazarovNikita1 <145589126+NazarovNikita1@users.noreply.github.com>
2023-10-17 18:46:05 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,16 +1,67 @@
-# This is a sample Python script.
+# задание 1
+def main():
+    score = float(input('Введите оценку по десятибальной шкале'))
+    if score == 10:
+        print('Ваша оценка по 5-бальной шкале: 5')
+    elif score >= 9:
+        print('Ваша оценка по 5-бальной шкале: 4')
+    elif score >= 8:
+        print('Ваша оценка по 5-бальной шкале: 3')
+    else:
+        print('Неудовлетворительная оценка')
 
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
 
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    main()
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+# задание 2
+def main():
+    age = int(input('Введите ваш возраст: '))
+
+    if age < 13:
+        print('Ребенок')
+    elif age < 18:
+        print('Подросток')
+    elif 18 <= age <= 25:
+        print('Юноша/девушка')
+    elif 26 <= age <= 60:
+        print('Зрелый человек')
+    elif age > 60:
+        print('Пожилой человек')
+if __name__ == '__main__':
+    main()
+
+
+# задание 3
+def greet(time):
+    if time < 6 or time >= 21:
+        return 'Доброй ночи!'
+    elif time <= 11:
+        return 'Доброе утро!'
+    elif time < 15:
+        return 'Добрый день!'
+    else:
+        return 'Доброго вечера!'
+
+hour = int(input())
+print(greet(hour))
+
+
+#задание 4
+import random
+
+number_of_tries = 0
+target_number = random.randint(1,100)
+print("Я загадал какое-то число от 1 до 100 - угадай его")
+while True:
+    number_of_tries += 1
+    user_input = int(input("Ваша догадка: "))
+    if user_input < target_number:
+        print("ваше число меньше загаданного")
+    elif user_input > target_number:
+        print ("Ваше число больше загаданного")
+
+print("ВЫ УГАДАЛИ!")
+print (f"Вы пытались {number_of_tries} раз.")
+print(f"Загаданное число: {target_number}")
